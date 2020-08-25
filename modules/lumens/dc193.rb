3e698ad6da3e564d12d93eae0728e5b5acbc6d1c
Stephen von Takach <steve@vontaka.ch>
2020-08-25 10:01:32 +1000

    feat(lumens dc193): match existing interface

--- modules/lumens/dc193.rb
+++ modules/lumens/dc193.rb
@@ -3,7 +3,7 @@ module Lumens; end
 # Documentation: https://aca.im/driver_docs/Lumens/DC193-Protocol.pdf
 # RS232 controlled device
 
-class Lumens::DC193
+class Lumens::Dc193
   include ::Orchestrator::Constants
   include ::Orchestrator::Transcoder
 
@@ -12,11 +12,10 @@ class Lumens::DC193
   descriptive_name "Lumens DC 193 Document Camera"
   generic_name :Visualiser
 
-  tokenize indicator: "\xA0", msg_length: 6
+  tokenize delimiter: "\xAF", indicator: "\xA0"
   delay between_sends: 100
 
   def on_load
-    @zoom_range = 0..@zoom_max
     self[:zoom_max] = 864
 		self[:zoom_min] = 0
 
@@ -26,6 +25,7 @@ class Lumens::DC193
     @lamp = false
     @head_led = false
     @frozen = false
+    @zoom_range = 0..@zoom_max
   end
 
   def connected
@@ -61,6 +61,8 @@ class Lumens::DC193
   end
 
   def lamp(state, head_led = false)
+    return if @frozen
+
     lamps = if state && head_led
               1
             elsif state
@@ -79,6 +81,8 @@ class Lumens::DC193
   end
 
   def zoom_to(position, auto_focus = true)
+    return if @frozen
+
     position = (position < 0 ? 0 : @zoom_max) unless @zoom_range.include?(position)
     low = (position & 0xFF)
     high = ((position >> 8) & 0xFF)
@@ -87,9 +91,11 @@ class Lumens::DC193
   end
 
   def zoom(direction)
+    return if @frozen
+
     case direction.to_s.downcase
     when "stop"
-      send [0xA0, 0x10, 0x00, 0x00, 0x00, 0xAF]
+      send [0xA0, 0x10, 0x00, 0x00, 0x00, 0xAF], name: :stop_zoom
       # Ensures this request is at the normal priority and ordering is preserved
       zoom?(priority: 50)
       # This prevents the auto-focus if someone starts zooming again
@@ -101,7 +107,20 @@ class Lumens::DC193
     end
   end
 
+  def zoom_in
+    zoom("in")
+  end
+
+  def zoom_out
+    zoom("out")
+  end
+
+  def zoom_stop
+    zoom("stop")
+  end
+
   def auto_focus(name = :auto_focus)
+    return if @frozen
     send [0xA0, 0xA3, 0x01, 0x00, 0x00, 0xAF], name: name
   end
 
@@ -114,11 +133,16 @@ class Lumens::DC193
     send [0xA0, 0x2C, state, 0x00, 0x00, 0xAF], name: :freeze
   end
 
+  def frozen(state)
+    freeze state
+  end
+
   def frozen?
     send [0xA0, 0x78, 0x00, 0x00, 0x00, 0xAF], priority: 0
   end
 
   def picture_mode(state)
+    return if @frozen
     mode = case state.to_s.downcase
            when "photo"
              0x00
@@ -130,6 +154,10 @@ class Lumens::DC193
              raise ArgumentError.new("unknown picture mode #{state}")
            end
     send [0xA0, 0xA7, mode, 0x00, 0x00, 0xAF], name: :picture_mode
+  end
+
+  def sharp(state)
+    picture_mode(state ? "text" : "photo")
   end
 
   def picture_mode?
@@ -164,31 +192,31 @@ class Lumens::DC193
     logger.debug { "DC193 sent: #{byte_to_hex data}" }
     data = str_to_array(data)
 
-    return :abort if (data[4] & 0x01) > 0
-    return :retry if (data[4] & 0x02) > 0
+    return :abort if (data[3] & 0x01) > 0
+    return :retry if (data[3] & 0x02) > 0
 
-    case COMMANDS[data[1]]
+    case COMMANDS[data[0]]
     when :power
-      data[2] == 0x01
+      data[1] == 0x01
     when :power_staus
-      @ready = data[2] == 0x01
-      @power = data[3] == 0x01
+      @ready = data[1] == 0x01
+      @power = data[2] == 0x01
       logger.debug { "System power: #{@power}, ready: #{@ready}" }
       self[:ready] = @ready
       self[:power] = @power
     when :max_zoom
-      @zoom_max = data[2] + (data[3] << 8)
+      @zoom_max = data[1] + (data[2] << 8)
       @zoom_range = 0..@zoom_max
       self[:zoom_range] = {min: 0, max: @zoom_max}
     when :frozen_status, :freeze
-      self[:frozen] = @frozen = data[2] == 1
+      self[:frozen] = @frozen = data[1] == 1
     when :zoom_staus, :zoom_direct_auto_focus, :zoom_direct
-      @zoom = data[2].to_i + (data[3] << 8)
+      @zoom = data[1].to_i + (data[2] << 8)
       self[:zoom] = @zoom
     when :picture_mode_staus, :picture_mode
-      self[:picture_mode] = PICTURE_MODES[data[2]]
+      self[:picture_mode] = PICTURE_MODES[data[1]]
     when :lamp_staus, :lamp
-      case data[2]
+      case data[1]
       when 0
         @head_led = @lamp = false
       when 1
@@ -205,7 +233,7 @@ class Lumens::DC193
     when :auto_focus
       # Can ignore this response
     else
-      error = "Unknown command #{data[1]}"
+      error = "Unknown command #{data[0]}"
       logger.debug { error }
       return :abort
     end
